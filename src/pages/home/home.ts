847b16334e95e5e5635ef9644397f4eed9134d1c
Neo <nsehlabane@gmail.com>
2018-10-23 14:40:35 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { Component} from '@angular/core';
+// import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from "@ionic-native/in-app-browser";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  url: string;
+  constructor(private inAppBrowser: InAppBrowser) { }
 
-  constructor(public navCtrl: NavController) {
+  openWebpage(url: string) {
+    const options: InAppBrowserOptions = {
+      zoom: 'no'
+    }
 
+    // Opening a URL and returning an InAppBrowserObject
+    const browser = this.inAppBrowser.create(url, '_self', options);
+
+   // Inject scripts, css and more with browser.X
   }
-
 }
